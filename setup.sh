b26058a8536ad2fcc1956ea48c86d5af801b083c
Maxim <maxim.hartmann@student.uni-tuebingen.de>
2026-06-25 01:45:31 +0200

    cleaned up Repo and corrected dependencies

--- setup.sh
+++ setup.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-
-echo "🧹 1. Bereinige requirements..."
 grep -v "^-e" requirements_cluster_exact.txt > requirements_clean.txt
-
-echo "🚀 2. Starte Singularity für das finale Setup..."
+uv sync
+rm -rf src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
+git clone https://github.com/g-truc/glm.git src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
 singularity exec --nv docker://pytorch/pytorch:2.1.0-cuda12.1-cudnn8-devel bash -c "
-    echo '📦 Erstelle frisches Environment (.venv_final)...'
-    python -m venv .venv_final
-    source .venv_final/bin/activate
-
-    echo '📥 Installiere exakte Pip-Pakete...'
-    pip install -r requirements_clean.txt
-
-    echo '🔧 Repariere GLM Mathe-Bibliothek...'
-    rm -rf src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
-    git clone https://github.com/g-truc/glm.git src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
-
-    echo '🔨 Kompiliere Rasterizer...'
+    source .venv/bin/activate
+    export CPATH=\$PWD/src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm:\$CPATH
     pip install -e src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/ --no-build-isolation
-
-    echo '🔨 Kompiliere Simple-KNN...'
     pip install -e src/gaussian-opacity-fields/submodules/simple-knn/ --no-build-isolation
 "
-
-echo "✅ Alles fertig installiert in '.venv_final'."
