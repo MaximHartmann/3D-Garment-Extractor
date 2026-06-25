d29ac4fc22d39b9fa207f591206ac41fedcca382
Maxim <maxim.hartmann@student.uni-tuebingen.de>
2026-06-25 12:54:48 +0200

    added test images, further refined setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-grep -v "^-e" requirements_cluster_exact.txt > requirements_clean.txt
 uv sync
 rm -rf src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
 git clone https://github.com/g-truc/glm.git src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/third_party/glm
@@ -9,3 +8,6 @@ singularity exec --nv docker://pytorch/pytorch:2.1.0-cuda12.1-cudnn8-devel bash 
     pip install -e src/gaussian-opacity-fields/submodules/diff-gaussian-rasterization/ --no-build-isolation
     pip install -e src/gaussian-opacity-fields/submodules/simple-knn/ --no-build-isolation
 "
+source .venv/bin/activate
+python download_models.py
+echo "Setup complete. Please run './run_inference.sh' to generate results."
