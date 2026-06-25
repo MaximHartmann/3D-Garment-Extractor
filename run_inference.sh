a049b77ea7b5bc381f53a8720ed3168c71083987
Maxim Hartmann <maxim.hartmann23@gmeil.com>
2026-06-25 13:40:49 +0200

    finalize setup and inference pipeline scripts

--- run_inference.sh
+++ run_inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 singularity exec --nv docker://pytorch/pytorch:2.1.0-cuda12.1-cudnn8-devel bash -c "
-    source .venv/bin/activate
+    export PYTHONPATH=\$PWD/.venv/lib/python3.10/site-packages:\$PYTHONPATH
     python src/Human3Diffusion/infer.py --test_imgs test_imgs --output output
 "
